4665f6786a4f717f4020bafabbd08da101acc431
Leoc74 <lciccarelli74@gmail.com>
2021-12-20 15:06:14 -0500

    added one piece of dummy data

---
+++ data/dummy_projects.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        "project_id" : 1,
+        "project_name" : "Izzo Painting",        
+        "project_type": "Web App",
+        "project_tech_stack" : "HTML, CSS, JavaScript",
+        "project_group_members" : "Leo, Evan, Chris",
+        "project_proposed_date": "April 6",
+        "project_description": "This is a website for local house painter Frank Izzo. He also does other home improvements, like staining and power washing."    
+    }
+    
+    
+
+]
