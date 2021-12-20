f913603f7e3668ef7314a9fb58bb185f397a8023
Leoc74 <lciccarelli74@gmail.com>
2021-12-20 15:13:01 -0500

    added another piece of dummy data

--- data/dummy_projects.js
+++ data/dummy_projects.js
@@ -8,7 +8,7 @@ module.exports = [
         "project_proposed_date": "April 6",
         "project_description": "This is a website for local house painter Frank Izzo. He also does other home improvements, like staining and power washing."
     },
-    
+
     {
         "project_id" : 2,
         "project_name" : "Garden China",        
@@ -17,6 +17,16 @@ module.exports = [
         "project_group_members" : "Ved, Jacob, Ryan",
         "project_proposed_date": "March 12",
         "project_description": "Garden China is a Chinese eatery located in Elmwood Park, NJ."
+    },
+
+    {
+        "project_id" : 3,
+        "project_name" : "More Foods Mod",        
+        "project_type": "Desktop",
+        "project_tech_stack" : "Java",
+        "project_group_members" : "Jeremy, Other_Evan, Other_Chris, Other_Leo",
+        "project_proposed_date": "May 4",
+        "project_description": "The More Foods Minecraft mod adds a bunch more foods to minecraft. There are crafting recipes to get the foods. These recipes involve existing and new foods."
     }
     
 
