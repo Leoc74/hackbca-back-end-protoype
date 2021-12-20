724ad99793b870d4433a4a7ec632a963cb1f30b3
Leoc74 <lciccarelli74@gmail.com>
2021-12-20 15:07:47 -0500

    added another piece of dummy data

--- data/dummy_projects.js
+++ data/dummy_projects.js
@@ -6,9 +6,18 @@ module.exports = [
         "project_tech_stack" : "HTML, CSS, JavaScript",
         "project_group_members" : "Leo, Evan, Chris",
         "project_proposed_date": "April 6",
-        "project_description": "This is a website for local house painter Frank Izzo. He also does other home improvements, like staining and power washing."    
-    }
+        "project_description": "This is a website for local house painter Frank Izzo. He also does other home improvements, like staining and power washing."
+    },
     
+    {
+        "project_id" : 2,
+        "project_name" : "Garden China",        
+        "project_type": "Web App",
+        "project_tech_stack" : "HTML, CSS, JavaScript",
+        "project_group_members" : "Ved, Jacob, Ryan",
+        "project_proposed_date": "March 12",
+        "project_description": "Garden China is a Chinese eatery located in Elmwood Park, NJ."
+    }
     
 
 ]
