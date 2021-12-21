7fc256727e043314a1b2e91b9c46614a490c921d
Leoc74 <lciccarelli74@gmail.com>
2021-12-21 14:44:07 -0500

    changed project_proposed_date to project_date_proposed

--- data/dummy_projects.js
+++ data/dummy_projects.js
@@ -5,7 +5,7 @@ module.exports = [
         "project_type": "Web App",
         "project_tech_stack" : "HTML, CSS, JavaScript",
         "project_group_members" : "Leo, Evan, Chris",
-        "project__date_proposed": "April 6",
+        "project_date_proposed": "April 6",
         "project_description": "This is a website for local house painter Frank Izzo. He also does other home improvements, like staining and power washing."
     },
 
@@ -15,7 +15,7 @@ module.exports = [
         "project_type": "Web App",
         "project_tech_stack" : "HTML, CSS, JavaScript",
         "project_group_members" : "Ved, Jacob, Ryan",
-        "project__date_proposed": "March 12",
+        "project_date_proposed": "March 12",
         "project_description": "Garden China is a Chinese eatery located in Elmwood Park, NJ."
     },
 
@@ -25,7 +25,7 @@ module.exports = [
         "project_type": "Desktop",
         "project_tech_stack" : "Java",
         "project_group_members" : "Jeremy, Other_Evan, Other_Chris, Other_Leo",
-        "project__date_proposed": "May 4",
+        "project_date_proposed": "May 4",
         "project_description": "The More Foods Minecraft mod adds a bunch more foods to minecraft. There are crafting recipes to get the foods. These recipes involve existing and new foods."
     }
     
